5f42ce22049c0f8f1bd60ffb32f77303360fb679
Ali <abidiar@gmail.com>
2023-12-29 09:53:39 -0500

    <changes to my database>

--- server/api/services/new-near-you/NewNearYou.sql
+++ server/api/services/new-near-you/NewNearYou.sql
@@ -1,7 +1,11 @@
-SELECT * FROM services
-WHERE ST_DWithin(
-    location,
-    ST_MakePoint(:longitude, :latitude)::GEOGRAPHY,
-    40233.6
-) AND date_added >= current_date - interval 'X days'
-ORDER BY date_added DESC;
+const query = `
+    SELECT * FROM services
+    WHERE ST_DWithin(
+        location,
+        ST_MakePoint($1, $2)::GEOGRAPHY,
+        40233.6
+    ) AND date_added >= current_date - interval '30 days'
+    ORDER BY date_added DESC;
+`;
+
+const values = [longitude, latitude]; 
