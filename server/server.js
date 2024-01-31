245e634a5fed82bf5ad818f5fe9b655ef0a1ed2b
Ali <abidiar@gmail.com>
2024-01-31 13:14:22 -0500

    <message describing your changes>

--- server/server.js
+++ server/server.js
@@ -39,11 +39,12 @@ app.get('/api/search', async (req, res) => {
     const { searchTerm, location, latitude, longitude, category, radius = 40233.6, sort = 'rank', isHalalCertified, page = 1, pageSize = 10 } = req.query;
 
     let baseSearchQuery = `
-      FROM services s
-      LEFT JOIN categories c ON s.category_id = c.id
-      WHERE to_tsvector('english', s.name || ' ' || s.description || ' ' || COALESCE(c.name, '')) @@ to_tsquery($1)
-    `;
-    
+    FROM services s
+    LEFT JOIN categories c ON s.category_id = c.id,
+    to_tsquery('english', $1) query,
+    to_tsvector('english', s.name || ' ' || s.description || ' ' || COALESCE(c.name, '')) textsearch
+    WHERE textsearch @@ query
+  `;
     let queryParams = [`${searchTerm}:*`];
 
     if (category) {
@@ -72,11 +73,12 @@ app.get('/api/search', async (req, res) => {
 
     // Pagination Query
     let paginatedSearchQuery = `
-      SELECT s.*, c.name AS category_name
-    ` + baseSearchQuery + `
-      ORDER BY ${sort === 'rating' ? 's.average_rating DESC' : sort === 'newest' ? 's.date_added DESC' : 'rank DESC'}
-      LIMIT $${queryParams.length + 1} OFFSET $${queryParams.length + 2}
-    `;
+    SELECT s.*, c.name AS category_name,
+    ts_rank_cd(textsearch, query) AS rank
+  ` + baseSearchQuery + `
+    ORDER BY ${sort === 'rating' ? 's.average_rating DESC' : sort === 'newest' ? 's.date_added DESC' : 'rank DESC'}
+    LIMIT $${queryParams.length + 1} OFFSET $${queryParams.length + 2}
+  `;
     queryParams.push(pageSize, (page - 1) * pageSize);
 
     // Execute the paginated search query
