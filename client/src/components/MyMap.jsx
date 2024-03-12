5eaa8d9abd1a68fc754534795d432d3614bb1480
abidiar <abidiar@gmail.com>
2024-03-12 06:52:26 -0400

    map update

--- client/src/components/MyMap.jsx
+++ client/src/components/MyMap.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
-const mapContainerStyle = {
-  height: "400px",
-  width: "100%"
-};
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Make sure to import the Leaflet CSS
 
 const Map = ({ businesses, center }) => {
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_GEO_API_KEY
-    }>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={12}
-      >
-        {businesses.map((business, index) => (
-          <Marker 
-            key={index} 
-            position={{ lat: parseFloat(business.latitude), lng: parseFloat(business.longitude) }}
-            // Optional: Include onClick here if you want to add some interaction when a marker is clicked
-          />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <MapContainer center={center} zoom={12} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {businesses.map((business, index) => (
+        <Marker 
+          key={index} 
+          position={[business.latitude, business.longitude]}
+        >
+          <Popup>
+            {business.name} // You can customize this Popup to show any information you want about the business
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
 
