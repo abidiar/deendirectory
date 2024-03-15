d8165960915efc2f19dcf73b45915f418f7e0662
abidiar <abidiar@gmail.com>
2024-03-15 16:02:32 -0400

    ds

--- client/src/services/supabaseClient.jsx
+++ client/src/services/supabaseClient.jsx
@@ -1,13 +1,14 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// Make sure these are the correct environment variable keys
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY;
+// Access environment variables using import.meta.env and the correct keys set in Render
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
 // Check if the environment variables are not undefined
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Supabase environment variables are not set or undefined.');
+  // You might want to handle this error more gracefully in a production environment
 } else {
   console.log('Supabase URL:', supabaseUrl); // For debugging purposes only; remove in production
 }
