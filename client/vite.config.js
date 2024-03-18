95efae596e300f2daeb72ae49661226c14ed5ab1
abidiar <abidiar@gmail.com>
2024-03-18 14:38:53 -0400

    dd

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'vite-plugin-dotenv';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dotenv()],
-  // If using custom prefixes for env variables
-  envPrefix: 'VITE_',
+  plugins: [react()],
 });
