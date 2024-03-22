6871787ddfb37cf9e2200f9b9f09a43560e9533e
abidiar <abidiar@gmail.com>
2024-03-22 07:17:49 -0400

    asas

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const businessDropdownRef = useRef(null);
 
   useEffect(() => {
-    setUser(supabase.auth.session());
+    setUser(supabase.auth.session);
     const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
       setUser(session);
     });
