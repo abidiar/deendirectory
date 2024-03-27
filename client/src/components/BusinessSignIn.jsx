e6a7cbee9e8217626277d5177eef6d3b77583209
abidiar <abidiar@gmail.com>
2024-03-27 12:19:58 -0400

    ass

--- client/src/components/BusinessSignIn.jsx
+++ client/src/components/BusinessSignIn.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { supabase } from '../services/supabaseClient';
 
 const BusinessSignIn = () => {
   const navigate = useNavigate();
--- client/src/components/UserSignIn.jsx
+++ client/src/components/UserSignIn.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../services/supabaseClient';
 
 const UserSignIn = () => {
   const [email, setEmail] = useState('');
