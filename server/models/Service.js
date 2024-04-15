7c03ce286dcd24b1a8e13c631e29470fcb0090fe
abidiar <abidiar@gmail.com>
2024-04-15 19:26:05 -0400

    Merge branch 'main' of https://github.com/abidiar/deendirectory

--- server/models/Service.js
+++ server/models/Service.js
@@ -1,100 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
 
-module.exports = (sequelize, models) => {
-  class Service extends Model {
-    static associate(models) {
-      // Define association here
-      Service.belongsTo(models.Category, {
-        foreignKey: 'categoryId',
-        as: 'category'
-      });
-    }
-  }
+class Service extends Model {}
 
-  Service.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    latitude: {
-      type: DataTypes.DECIMAL(10, 8),
-      allowNull: false,
-    },
-    longitude: {
-      type: DataTypes.DECIMAL(11, 8),
-      allowNull: false,
-    },
-    location: {
-      type: DataTypes.GEOGRAPHY('POINT'),
-      allowNull: true,
-    },
-    dateAdded: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      field: 'date_added',
-    },
-    categoryId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'categories',
-        key: 'id',
-      },
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
-      field: 'category_id',
-    },
-    streetAddress: {
-      type: DataTypes.STRING,
-      field: 'street_address',
-    },
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    postalCode: {
-      type: DataTypes.STRING,
-      field: 'postal_code',
-    },
-    country: DataTypes.STRING,
-    phoneNumber: {
-      type: DataTypes.STRING,
-      field: 'phone_number',
-    },
-    website: DataTypes.STRING,
-    hours: DataTypes.STRING,
-    isHalalCertified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      field: 'is_halal_certified',
-    },
-    averageRating: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0,
-      field: 'average_rating',
-    },
-    reviewCount: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      field: 'review_count',
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      field: 'image_url',
-    },
-  }, {
-    sequelize,
-    modelName: 'Service',
-    tableName: 'services',
-    timestamps: false,
-    underscored: true,
-  });
+Service.init({
+  // Model attributes definition
+}, {
+  sequelize,
+  modelName: 'Service'
+  // other model options
+});
 
-  return Service;
-};
+module.exports = Service;
--- server/models/index.js
+++ server/models/index.js
@@ -1,31 +1,7 @@
-'use strict';
+const sequelize = require('../db/sequelize');
+const Service = require('./Service');
 
-const { Sequelize } = require('sequelize');
-const sequelize = require('../db/sequelize'); // Adjust the path to where your configured sequelize instance is located
-
-// Import model definitions as functions
-const defineService = require('../models/Service');
-const defineCategory = require('../models/Category');
-
-// Initialize models by invoking the imported functions
-const Service = defineService(sequelize, Sequelize.DataTypes);
-const Category = defineCategory(sequelize, Sequelize.DataTypes);
-
-// Setup associations
-const models = {
-  Service,
-  Category
-};
-
-Object.keys(models).forEach(modelName => {
-  if (models[modelName].associate) {
-    models[modelName].associate(models);
-  }
-});
-
-// Export the sequelize instance and models
 module.exports = {
-  sequelize, // the configured sequelize instance
-  Sequelize, // the Sequelize class
-  ...models
+  Service,
+  // other models
 };
