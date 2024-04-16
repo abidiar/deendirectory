16beb5e4dd648d9eeda90204a918b826ab6ee2ba
abidiar <abidiar@gmail.com>
2024-04-15 20:13:22 -0400

    dd

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,13 @@ const sequelize = require('../db/sequelize');
 const Service = require('./Service')(sequelize);
 const Category = require('./Category')(sequelize);
 
+// Set up associations
+Object.keys(sequelize.models).forEach((modelName) => {
+  if (sequelize.models[modelName].associate) {
+    sequelize.models[modelName].associate(sequelize.models);
+  }
+});
+
 module.exports = {
   sequelize,
   Service,
